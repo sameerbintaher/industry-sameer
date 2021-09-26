cd7f64a237e6833f9452a1477e4535ea9dda99e8
sameerbintaher <sameerbintaher@gmail.com>
2021-09-26 11:28:39 +0600

    db changed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="bg-dark text-white p-3">
-            <h1>SamPro Music And Record Industry</h1>
-            <h2>Pick a singer and make your awesome wishlist</h2>
+            <h1>SamPro Film And Music Industry</h1>
+            <h2>Pick a person and make your awesome wishlist</h2>
             <h1>Total Revenue: $20000</h1>
         </div>
     );
